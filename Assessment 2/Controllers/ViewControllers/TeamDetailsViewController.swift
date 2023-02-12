ce8554e10059d72e72e35c7f1fb52f41610c2880
Chase <chaseaohill@gmail.com>
2023-02-11 23:25:18 -0500

    Completed save button tapped function and working on clear and delete functions.

--- Assessment 2/Controllers/ViewControllers/TeamDetailsViewController.swift
+++ Assessment 2/Controllers/ViewControllers/TeamDetailsViewController.swift
@@ -11,6 +11,15 @@ class TeamDetailedViewController: UIViewController {
     
     // MARK: - TextField and Button Outlets
     
+    @IBOutlet weak var teamNameTextField: UITextField!
+    
+    @IBOutlet weak var teamRankingTextField: UITextField!
+    
+    @IBOutlet weak var teamCountTextField: UITextField!
+    
+    @IBOutlet weak var clearButton: UIButton!
+    
+    @IBOutlet weak var deleteButton: UIButton!
     
     // MARK: - Life-Cycle
     override func viewDidLoad() {
@@ -18,20 +27,46 @@ class TeamDetailedViewController: UIViewController {
         
     }
     
+    // MARK: - Property
+    
     var team: Team?
     
+    // MARK: - Button Actions
     
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let name = teamNameTextField.text, !name.isEmpty,
+        let ranking = teamRankingTextField.text, !"\(ranking)".isEmpty,
+              let count = teamCountTextField.text, !"\(count)".isEmpty else {return}
+        
+        let unwrappedRanking = Double(ranking) ?? 0.0
+        let unwrappedCount = Int(count) ?? 0
+        
+        if let team = team {
+            TeamController.sharedInstance.updateTeam(teamToUpdate: team, nameToUpdate: name, ratingToUpdate: unwrappedRanking, countToUpdate: unwrappedCount)
     
+        } else {
+            
+            TeamController.sharedInstance.createTeam(name: name, rating: unwrappedRanking, count: unwrappedCount)
+            
+        }
+        
+        navigationController?.popViewController(animated: true)
+        
+    }
     
+    @IBAction func deleteButtonTapped(_ sender: Any) {
+    }
     
+    @IBAction func clearButtonTapped(_ sender: Any) {
+        resetView()
+    }
     
+    // MARK: - Helper Functions for resetting and updating the view
     
-    
-    
-    
-    
-    
-    
-    
-    
+    func updateView(team: Team?) {
+        guard let team = team else {return}
+        teamNameTextField.text = team.teamName
+        teamRankingTextField.text = team.teamRating
+        teamCountTextField.text = team.teamCount
+    }
 }
